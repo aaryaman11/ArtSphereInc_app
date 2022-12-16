220ce21e0bf4480d031f3f042d1b9238f4afb545
PrithviRana1 <pzr5169@psu.edu>
2022-12-15 19:53:31 -0500

    Updating changes

---
+++ app/src/main/java/com/example/artsphereincapp/OnBoarding.java
@@ -0,0 +1,17 @@
+package com.example.artsphereincapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OnBoarding extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding_activity);
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/artsphereincapp/signupActivity.java
+++ app/src/main/java/com/example/artsphereincapp/signupActivity.java
@@ -2,7 +2,10 @@ package com.example.artsphereincapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class signupActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class signupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button longterm = (Button) findViewById(R.id.Longterm);
+
+        longterm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signupActivity.this, OnBoarding.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
+
+
+
 }
