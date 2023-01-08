47e155ca28d348f42a1304fca35fa702e8bf64bb
Aaryaman <Aaryaman.Sharma@artsphereinc.org>
2023-01-07 18:54:21 -0800

    Testing github commit

--- app/src/main/java/com/example/artsphereincapp/SurveyActivity.java
+++ app/src/main/java/com/example/artsphereincapp/SurveyActivity.java
@@ -12,7 +12,7 @@ public class SurveyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-        thank;
+
 
     }
 }
